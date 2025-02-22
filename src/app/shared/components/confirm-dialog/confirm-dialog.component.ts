355729991afebd8eedf4a47246122ceb79581597
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2025-02-23 02:38:17 +0400

    Created confirm dialog component

---
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,29 @@
+// shared/components/confirm-dialog/confirm-dialog.component.ts
+import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+interface DialogData {
+  title: string;
+  message: string;
+}
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss',
+})
+export class ConfirmDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+}
