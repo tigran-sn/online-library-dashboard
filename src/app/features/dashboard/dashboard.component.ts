591b77d8ac4080216d74b59246f95eb58d10e846
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2025-02-25 02:52:35 +0400

    Handled the initial loading state and potential errors in dashboard component

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -21,13 +21,18 @@ export class DashboardComponent {
   private productService = inject(ProductService);
   private personService = inject(PersonService);
 
-  private users = toSignal(this.userService.getUsers());
-  private products = toSignal(this.productService.getProducts());
-  private persons = toSignal(this.personService.getPersons());
-
-  protected usersCount = computed(() => this.users()?.total);
-  protected productsCount = computed(() => this.products()?.total);
-  protected personsCount = computed(() => this.persons()?.total);
+  protected users = toSignal(this.userService.getUsers(), {
+    initialValue: null,
+  });
+  protected products = toSignal(this.productService.getProducts(), {
+    initialValue: null,
+  });
+  protected persons = toSignal(this.personService.getPersons(), {
+    initialValue: null,
+  });
+  protected usersCount = computed(() => this.users()?.total ?? 0);
+  protected productsCount = computed(() => this.products()?.total ?? 0);
+  protected personsCount = computed(() => this.persons()?.total ?? 0);
 
   navigateTo(route: string) {
     this.router.navigate([route]);
