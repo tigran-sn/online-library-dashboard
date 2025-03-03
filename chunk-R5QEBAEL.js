039f921168b1153a6552629d8a605660adf5c0fe
tigran-sn <soghomonyantigran@gmail.com>
2025-03-03 18:44:37 +0000

    Deploying to gh-pages from @ tigran-sn/online-library-dashboard@ad81b73570c5ca6836b988d6a0d0adea308821f7 🚀

---
+++ chunk-R5QEBAEL.js
@@ -0,0 +1,1 @@
+import{Z as i}from"./chunk-EERKQ2IA.js";var f=class o{getInitials(r,t,c="?"){let n=r&&r.length>0?r.charAt(0):"",e=t&&t.length>0?t.charAt(0):"";return!n&&!e?c:`${n}${e}`.toUpperCase()}static \u0275fac=function(t){return new(t||o)};static \u0275prov=i({token:o,factory:o.\u0275fac,providedIn:"root"})};export{f as a};
